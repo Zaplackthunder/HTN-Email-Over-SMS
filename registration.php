4144dd2f5846e3b85ec6f58bae9daa0ec803478c
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2014-09-20 18:04:58 -0400

    updated with throwaway gmail and added phonenumber persistence

--- registration.php
+++ registration.php
@@ -1,15 +1,55 @@
+<?php
+
+	require_once 'Google/Client.php';
+	require_once 'Google/Service/Gmail.php';
+
+	/************************************************
+	  ATTENTION: Fill in these values!
+	 ************************************************/
+	 $client_id = '144543803350-sbqtalh9g4m8pmpqc3k6dhvaa09vkhb3.apps.googleusercontent.com';
+	 $client_secret = 'PIDEfDLhtvzFi3_GHLUI0EF1';
+	 $redirect_uri = 'http://localhost/thankyou.php';
+
+	/************************************************
+	  Make an API request on behalf of a user. In
+	  this case we need to have a valid OAuth 2.0
+	  token for the user, so we need to send them
+	  through a login flow. To do this we need some
+	  information from our API console project.
+	 ************************************************/
+	$client = new Google_Client();
+	$client->setApplicationName("Hack The North - SMS Email");
+	$client->setClientId($client_id);
+	$client->setClientSecret($client_secret);
+	$client->setRedirectUri($redirect_uri);
+	$client->setAccessType('offline');
+	$client->addScope("https://www.googleapis.com/auth/gmail.readonly");
+	$client->addScope("https://www.googleapis.com/auth/gmail.compose");
+
+	$authUrl = $client->createAuthUrl();
+
+	if ( !isset($_SESSION) ) {
+		session_start();
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 	</head>
 	<body>
 		<h2>Please fill in your information</h2>
-		<form action="thankyou.php" method="post">
+		<form action="registration.php" method="post">
 			Phone Number: <input type="text" name="phonenumber"><br>
-			Email Account: <input type="email" name="email"><br>
-			Email Password: <input type="password" name="emailpassword"><br>
-			Confirm Password: <input type="password" name="confirmpassword"><br>
-			<input type="submit" value="Submit">
+			<input type="submit" value="submit">Submit</input>
 		</form>
 	</body>
 </html>
+
+<?php
+	if ( isset($_POST['phonenumber']) ) {
+		$_SESSION['phonenumber'] = $_POST['phonenumber'];
+		header("Location: " . $authUrl);
+	}
+?>
--- thankyou.php
+++ thankyou.php
@@ -1,16 +1,33 @@
 <?php
-	$con=mysqli_connect("localhost","root","1234","my_db");
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+if ( !isset($_SESSION) ) {
+	session_start();
+}
+
+function getData() {
+	if ( isset($_REQUEST['code']) ) {
+		$url = "https://accounts.google.com/o/oauth2/token";
+		$ch = curl_init();
+		$post_data = array(
+			"code" => $_REQUEST['code'],
+			"client_id" => "1088708870228-e23b8avbg0v1dfc20b44ltmsksm2048m.apps.googleusercontent.com",
+			"client_secret" => "peH484fr-FyGhisJodNLLqz3",
+			"redirect_uri" => "http://localhost/thankyou.php",
+			"grant_type" => "authorization_code"
+		);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$stringResult = curl_exec($ch);
+		$result = json_decode($stringResult, true);
+		$result['phonenumber'] = $_SESSION['phonenumber'];
+		return $result;
 	}
-	$pnum=$_POST['phonenumber'];
-	$mailaddr=$_POST['email'];
-	$pword=$_POST['emailpassword'];
-	
-	$sql="INSERT INTO MailList (Phone, Mail, Password, MenuStatus) VALUES ('$pnum', '$mailaddr','$pword', 0)";
-	if (!mysqli_query($con,$sql)) {
-		die('Error: ' . mysqli_error($con));
-	}
-	mysqli_close($con);
-	?>
+	return false;
+}
+
+?>
 	
