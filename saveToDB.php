3804ebab0de9b96b89bd2f7a97b5c8c1c44c7b03
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2014-09-20 18:57:40 -0400

    changed localhost to SERVER[HTTP_HOST], fixed bugs

--- saveToDB.php
+++ saveToDB.php
@@ -1,6 +1,6 @@
 <?php
 	function toDB($object){
-		$con=mysqli_connect("localhost","root","1234","my_db");
+		$con=mysqli_connect($_SERVER['HTTP_HOST'],"root","1234","my_db");
 		if (mysqli_connect_errno()) {
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
@@ -10,7 +10,7 @@
 		$renewtoken=$object->$refreshtoken;
 		$phpdate=date_create();
 		$mysqldate = date( 'Y-m-d H:i:s', $phpdate );
-		sql="INSERT INTO MailList (Phone, Mail, RefreshToken, AccessToken, LastUpdated) VALUES ('$pnum', '$mailaddr', '$renewtoken', '$accesstoken', '$mysqldate')";
+		$sql = "INSERT INTO MailList (Phone, Mail, RefreshToken, AccessToken, LastUpdated) VALUES ('$pnum', '$mailaddr', '$renewtoken', '$accesstoken', '$mysqldate')";
 		if (!mysqli_query($con,$sql)){
 			die('Error: ' . mysqli_error($con));
 		}
